f3c66ba8fd615dcea40b1a634d1f6acde7264a7c
LukeDubs <116839482+LukeDubs@users.noreply.github.com>
2023-07-03 23:03:45 +0100

    Solve 04 exercise Answer worked, but was not the same as the solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    let newArr = [];
+    for (let i = 0; i < array.length; i++) {
+      if (!toRemove.includes(array[i])) {
+        newArr.push(array[i]);
+      }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
