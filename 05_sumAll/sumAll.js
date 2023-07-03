33ebce763aa3cb5833ffcc9b98005c0e1044601e
LukeDubs <116839482+LukeDubs@users.noreply.github.com>
2023-07-03 23:28:13 +0100

    Solve 05 Solution was different but worked

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (oneInt, twoInt) {
+    if (typeof oneInt !== 'number' || typeof twoInt !== 'number' || 
+         oneInt < 0 || twoInt < 0) {
+        return 'ERROR';
+    } else {
+        let sum = 0;
+        let lower = Math.min(oneInt, twoInt);
+        let upper = Math.max(oneInt, twoInt);
 
+        for (let i = lower; i <= upper; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
